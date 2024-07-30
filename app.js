afc56c155e0867edb02db8b43b0af45b2ea00260
lyklecharova <lklecharova9@gmail.com>
2024-07-30 16:42:14 +0300

    v-bind:  Dynamic link

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'https://vuejs.org/',
             showBooks: true,
             books:[
                 {title: 'name of wind',author: 'patric'},
