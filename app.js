aab8682116b80ec605b30920670bc2cead7afb8d
lyklecharova <lklecharova9@gmail.com>
2024-07-30 17:03:52 +0300

    dynamic classes

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = Vue.createApp({
         return {
             url: 'https://vuejs.org/',
             showBooks: true,
-            books:[
-                {title: 'name of wind',author: 'patric', img:'img/1.jpg'},
-                {title: 'the way of kings',author: 'brandon', img:'img/2.jpg'},
-                {title: 'the final empire',author: 'brandon', img:'img/3.jpg'},
+            books: [
+                { title: 'name of wind', author: 'patric', img: 'img/1.jpg', isFav: true },
+                { title: 'the way of kings', author: 'brandon', img: 'img/2.jpg', isFav: false },
+                { title: 'the final empire', author: 'brandon', img: 'img/3.jpg', isFav: true },
             ]
             // title: 'The Final Empire',
             // author: 'Brandon Sanderson',
@@ -25,11 +25,11 @@ const app = Vue.createApp({
         },
         handleEvent(e, data) {
             console.log(e, e.type);
-            if(data){
+            if (data) {
                 console.log(data);
             }
         },
-        handleMousemove(e){
+        handleMousemove(e) {
             this.x = e.offsetX;
             this.y = e.offsetY;
         }
