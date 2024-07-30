53443be50423c3ebb4655c0f7e325a2ce46cab5b
lyklecharova <lklecharova9@gmail.com>
2024-07-30 16:23:27 +0300

    mouse click

---
+++ app.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 35,
+            x:0,
+            y:0
+        };
+    },
+    methods: {
+        // changeTitle(title){
+        // //    this.title = 'Words of Radiance'
+        // this.title = title
+        // }
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type);
+            if(data){
+                console.log(data);
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        }
+    }
+});
+
+app.mount('#app');
