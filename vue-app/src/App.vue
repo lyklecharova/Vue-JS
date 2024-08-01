7ab749b666111c5ea0b2e47966c71c290971844e
lyklecharova <lklecharova9@gmail.com>
2024-08-01 17:30:41 +0300

    addTask  function
    create form input

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,47 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div>
+        <h1>{{ name }}</h1>
+        <p v-if="status === 'active'">User is active</p>
+        <p v-else-if="status === 'pending'">User is pending</p>
+        <p v-else>User is inactive</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <form @submit.prevent="addTask">
+            <label for="newTask">Add Task</label>
+            <input 
+                type="text" 
+                name="newTask" 
+                id="newTask" 
+                v-model="newTask"
+            />
+            <button type="submit">Submit</button>
+        </form>
+        <h3>Tasks:</h3>
+        <ul>
+            <li v-for="task in tasks" :key="task">{{ task }}</li>
+        </ul>
+        <br />
+        <button @click="toggleStatus">Change Status</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import { ref } from "vue";
+
+const name = ref("John Doe");
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task Three"]);
+const newTask = ref("");
+
+const toggleStatus = () => {
+    if (status.value == "active") {
+        status.value = "pending";
+    } else if (status.value === "pending") {
+        status.value = "inactive";
+    } else {
+        status.value = "active";
+    }
+};
+const addTask = () =>{
+    if(newTask.value.trim() !== ''){
+        tasks.value.push(newTask.value);
+        newTask.value = '';
+    }
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
