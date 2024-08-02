25f2b3efb230a6f6f2d6ac062379d04e275f6d3e
lyklecharova <lklecharova9@gmail.com>
2024-08-02 18:50:17 +0300

    Update tailwind.config.js

--- vue-app/tailwind.config.js
+++ vue-app/tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue, js, ts, jsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      fontFamily:{
-        sans: ['Popins','sans-serif']
+      fontFamily: {
+        sans: ['Popins', 'sans-serif']
       },
-      gridTemplateColumns:{
+      gridTemplateColumns: {
         '70/30': '70% 28%'
       },
     },
-    variants:{
-      extend:{},
+    variants: {
+      extend: {},
     },
   },
   plugins: [],
