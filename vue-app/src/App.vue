8b7e6afd7a7a7a44f31e59d2ec1d5f20e5d5481a
lyklecharova <lklecharova9@gmail.com>
2024-08-02 18:50:38 +0300

    Update App.vue

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,18 +1,11 @@
 <template>
     <div>
         <Navbar />
-        <Hero />
-        <HomeCards />
-        <JobListings 
-            :limit="3"
-            :showButton="true"
-        />
+        <RouterView />
     </div>
 </template>
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
-import Hero from "@/components/Hero.vue";
-import HomeCards from "@/components/HomeCards.vue";
-import JobListings from "@/components/JobListings.vue";
+import { RouterView } from "vue-router";
 </script>
