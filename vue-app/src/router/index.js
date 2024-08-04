bb2baca0d83cf97774e17fb8097fbc201c1899f8
lyklecharova <lklecharova9@gmail.com>
2024-08-04 11:26:08 +0300

    auth guard about edit page

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -4,6 +4,7 @@ import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,21 +14,31 @@ const router = createRouter({
             name: 'home',
             component: HomeView,
         },
+
         {
             path: '/jobs',
             name: 'jobs',
             component: JobsView,
         },
+
         {
             path: '/jobs/:id',
             name: 'job',
             component: JobView,
         },
+
         {
             path: '/jobs/add',
             name: 'add-job',
             component: AddJobView,
         },
+
+        {
+            path: '/jobs/edit/:id',
+            name: 'edit-job',
+            component: EditJobView,
+        },
+
         {
             path: '/:catchAll(.*)',
             name: 'not-found',
