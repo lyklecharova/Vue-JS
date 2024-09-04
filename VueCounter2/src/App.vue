3a7a31c9b2cf0e06fc5a94465c67f6abc925975a
lyklecharova <lklecharova9@gmail.com>
2024-09-04 18:37:43 +0300

    Create counter with VueJS (another way)

---
+++ VueCounter2/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <main>
+        <div>
+            <h4>The current count is...</h4>
+            <h1>{{ count }}</h1>
+            <button @click="substractFromCount()">-</button>
+            <button @click="addToCount()">+</button>
+        </div>
+    </main>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+const addToCount = () => {
+    count.value = count.value + 1;
+};
+
+const substractFromCount = () => {
+    count.value = count.value - 1;
+};
+</script>
+
+<style scoped>
+main {
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+div {
+    text-align: center;
+}
+</style>
