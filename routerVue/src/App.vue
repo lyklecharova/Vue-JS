def3f02c6149325fb07795ef7a82971f6354ef57
lyklecharova <lklecharova9@gmail.com>
2024-09-06 12:11:51 +0300

    create active link

--- routerVue/src/App.vue
+++ routerVue/src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <Nav/>
-   <RouterView/>
-  </div>
+    <div>
+        <Nav />
+        <RouterView />
+    </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
 import Nav from './components/Nav.vue';
+
 </script>
--- routerVue/src/components/Nav.vue
+++ routerVue/src/components/Nav.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
+        <RouterLink active-class="active" to="/">Home</RouterLink>
+        <RouterLink active-class="active" to="/about">About</RouterLink>
     </div>
 </template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style scoped>
+.active {
+    font-weight: 900;
+    color: wheat;
+}
+</style>
