ac24c698fbd509fdaf5fa3fbfa278b2dd9f639b9
lyklecharova <lklecharova9@gmail.com>
2024-09-06 11:53:44 +0300

    routerView

---
+++ routerVue/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <Nav/>
+   <RouterView/>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import Nav from './components/Nav.vue';
+</script>
